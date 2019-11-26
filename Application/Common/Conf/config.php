2f4ed92949c48b7c419715c18ef188a30932c1e1
王晓宁 <xiaoning1@botpy.com>
2019-11-26 10:14:57 +0800

    路由

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,8 +7,13 @@ return array(
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => 'localhost', // 服务器地址
     'DB_NAME'   => 'typecho', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'wyt05223775', // 密码
+//    'DB_USER'   => 'root', // 用户名
+//    'DB_PWD'    => 'wyt05223775', // 密码
+
+    'DB_USER'   => 'typecho', // 用户名
+    'DB_PWD'    => 'BHjy6L2KNLJmKB3Y', // 密码
+
+
     'DB_PORT'   => 3306, // 端口
     'DB_PREFIX' => 'typecho_', // 数据库表前缀
     'DB_CHARSET'=> 'utf8', // 字符集
