c5e28c2e3f69952904cb8c6148e29a10c6bf34e0
王晓宁 <xiaoning1@botpy.com>
2019-11-26 14:53:37 +0800

    commit

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -10,12 +10,18 @@ class IndexController extends Controller
     {
         $show_type = [
             ['type' => 'logo','method' => 'find','field' => 'path'],
+            ['type' => 'app','method' => 'select','field' => 'name_ch,path'],
+            ['type' => 'leimu','method' => 'select','field' => 'name_ch,path'],
+            ['type' => 'index_image','method' => 'select','field' => 'path,image_path'],
+            ['type' => 'roll_article','method' => 'select','field' => 'path,image_path,short_title'],
+            ['type' => 'f1_article','method' => 'select','field' => 'path,image_path,short_title','limit' => 6],
+            ['type' => 'footer1','method' => 'select','field' => 'path,image_path,short_title','limit' => 6],
         ];
         $config = [];
         foreach ($show_type as $item){
-            $config[$item['type']] = M('config')->where(['type' => $item['type']])->field($item['field'])->{$item['method']}();
+            $config[$item['type']] = M('config')->where(['type' => $item['type']])->limit(6)->field($item['field'])->{$item['method']}();
         }
-
+//        dump($config);die;
         $this->assign('config', $config);
         $this->display();
     }
