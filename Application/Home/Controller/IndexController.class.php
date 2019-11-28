30d38d310a6fe116070c31b9080e4b47b61a4084
王晓宁 <xiaoning1@botpy.com>
2019-11-28 11:02:20 +0800

    admin

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,19 +6,20 @@ use Think\Controller;
 
 class IndexController extends Controller
 {
+    private $show_type = [
+        ['type' => 'logo','method' => 'find','field' => 'path,name_ch,id'],
+        ['type' => 'app','method' => 'select','field' => 'name_ch,path,id'],
+        ['type' => 'leimu','method' => 'select','field' => 'name_ch,path,id'],
+        ['type' => 'index_image','method' => 'select','field' => 'path,image_path,name_ch,id'],
+        ['type' => 'roll_article','method' => 'select','field' => 'path,image_path,short_title,name_ch,id'],
+        ['type' => 'f1_article','method' => 'select','field' => 'path,image_path,short_title,name_ch,id','limit' => 6],
+        ['type' => 'footer1','method' => 'select','field' => 'path,image_path,short_title,name_ch,id','limit' => 6],
+    ];
+
     public function index()
     {
-        $show_type = [
-            ['type' => 'logo','method' => 'find','field' => 'path'],
-            ['type' => 'app','method' => 'select','field' => 'name_ch,path'],
-            ['type' => 'leimu','method' => 'select','field' => 'name_ch,path'],
-            ['type' => 'index_image','method' => 'select','field' => 'path,image_path'],
-            ['type' => 'roll_article','method' => 'select','field' => 'path,image_path,short_title'],
-            ['type' => 'f1_article','method' => 'select','field' => 'path,image_path,short_title','limit' => 6],
-            ['type' => 'footer1','method' => 'select','field' => 'path,image_path,short_title','limit' => 6],
-        ];
         $config = [];
-        foreach ($show_type as $item){
+        foreach ($this->show_type as $item){
             $config[$item['type']] = M('config')->where(['type' => $item['type']])->limit(6)->field($item['field'])->{$item['method']}();
         }
 
@@ -34,13 +35,12 @@ class IndexController extends Controller
 
     public function SY5uVsuZB()
     {
-        $config = M('config')->select();
-        $return_data = [];
-        foreach ($config as $item){
-            $return_data[$item['type']][] = $item;
+        $config = [];
+        foreach ($this->show_type as $item){
+            $config[$item['type']] = M('config')->where(['type' => $item['type']])->limit(6)->field($item['field'])->{$item['method']}();
         }
-
-        $this->assign('config', $return_data);
+//        dump($config);die;
+        $this->assign('config', $config);
         $this->display();
     }
 
@@ -53,6 +53,11 @@ class IndexController extends Controller
 
     public function IbTjazNOh()
     {
-
+        $id = isset($_REQUEST['id']) ? $_REQUEST['id']:0;
+        $save['path'] = isset($_REQUEST['path']) ? $_REQUEST['path']:0;
+        $save['image_path'] = isset($_REQUEST['image_path']) ? $_REQUEST['image_path']:0;
+        $save['short_title'] = isset($_REQUEST['short_title']) ? $_REQUEST['short_title']:0;
+        $save['add_time'] = date("Y-m-d H:i:s");
+        M('config')->where(['id'=>$id])->save($save);
     }
 }
